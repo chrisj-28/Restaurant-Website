7466b41a54e09d610f6702439d5565cffc4166f5
Chris Jiang <c74jiang@uwaterloo.ca>
2021-01-25 17:03:49 -0800

    second commit, mobile friendly

--- beijingBistro.js
+++ beijingBistro.js
@@ -1,7 +1,32 @@
-function toggleNavigationBar() {
-    var x = document.getElementById("homeTopNav");
-    if (x.className == "topNav") 
-        x.className += " responsive";
-    else   
-        x.className = "topNav";
+/*$(document).ready(function() {
+    var windowWidth = $(window).width();
+    if (windowWidth < 100) {
+        alert("hola");
+    }
+});
+
+$(window).resize(function() {
+    var windowWidth = $(window).width();
+    if (windowWidth <= 400) {
+        $("html").css("background-size", "auto 250px");
+    }
+    else {
+        $("html").css("background-size", "100% auto");
+    }
+});*/
+
+
+
+function toggleNavigationBar(button) {
+
+    if (button.classList.contains("navToggleOn")) {
+        document.getElementById("sideNav").style.width = "0px";
+        document.body.style.backgroundColor = "rgba(0,0,0,0)";
+    } else {
+        document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+        document.getElementById("sideNav").style.width = "250px";
+    }
+
+    button.classList.toggle("navToggleOn")
+
 }
